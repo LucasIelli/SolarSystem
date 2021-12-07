1303e6620165ddfb9ad99b6f17e7a12523943ed9
LucasIelli <lucas.ielli.pro@gmail.com>
2021-12-07 12:03:00 +0100

    webpack.prod.js change path into docs for github page pusblish

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -1,14 +1,13 @@
-const { merge } = require('webpack-merge')
-const commonConfiguration = require('./webpack.common.js')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { merge } = require("webpack-merge");
+const commonConfiguration = require("./webpack.common.js");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
-module.exports = merge(
-    commonConfiguration,
-    {
-        mode: 'production',
-        plugins:
-        [
-            new CleanWebpackPlugin()
-        ]
-    }
-)
+module.exports = merge(commonConfiguration, {
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+  output: {
+    path: path.resolve(__dirname, "../docs"),
+    filename: "bundle.js",
+  },
+});
